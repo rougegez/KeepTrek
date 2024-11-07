9e046b8b69d977a7daf6bd9540c8bb2b9f538ed4
Kyle Goh <kylegoh12@gmail.com>
2024-11-07 10:24:57 +0800

    NotesAdded

--- KeepTrek/src/components/TripDetails/Notes/Notes.jsx
+++ KeepTrek/src/components/TripDetails/Notes/Notes.jsx
@@ -1,11 +1,38 @@
-import React from 'react';
-import './Notes.css'
+import React, { useState, useRef } from 'react';
+import styles from './Notes.module.css';
+import { Plus } from 'lucide-react'
 
 const Notes = () => {
+  const [note, setNote] = useState('');
+  const textareaRef = useRef(null);
+
+  const handleInputChange = (e) => {
+    setNote(e.target.value);
+    autoResizeTextarea();
+  };
+
+  const autoResizeTextarea = () => {
+    const textarea = textareaRef.current;
+    if (textarea) {
+      textarea.style.height = 'auto'; // Reset height to calculate the new scroll height
+      textarea.style.height = `${textarea.scrollHeight}px`; // Set height based on content
+    }
+  };
+
   return (
-    <div className="notes">
-      <h3>Notes</h3>
-      <textarea placeholder="Write or paste anything here." />
+    <div className={styles.noteContainer}>
+      <div className={styles.noteHeader}>
+        <h2 className={styles.noteTitle}>Notes</h2>
+        <button className={styles.addNoteButton}><Plus/></button>
+      </div>
+      <textarea
+        ref={textareaRef}
+        className={styles.inputNote}
+        placeholder="Type your notes here..."
+        value={note}
+        onChange={handleInputChange}
+        rows="1" // Minimum number of rows
+      />
     </div>
   );
 };
