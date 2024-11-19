f72faae25f358f6e38a2afe11b6f432170f460f8
rougegez <rougetgc@gmail.com>
2024-11-20 04:18:02 +0800

    itinerary page front end

--- KeepTrek/src/components/Sidebar/Sidebar.jsx
+++ KeepTrek/src/components/Sidebar/Sidebar.jsx
@@ -135,7 +135,7 @@ export const AppSidebar = () => {
                 "absolute flex left-2 w-[calc(100%-1rem)] h-[2.5rem] bg-[#ff004f] rounded-[12px] items-center justify-start pl-4 pt-1"
                  : 
                 ""}`}
-                to="/itinerary">Itinerary</NavLink>
+                to="/itineraryWL">Itinerary</NavLink>
                 <ChevronDown className="ml-auto transition-transform group-data-[state=open]/collapsible:rotate-180" />
               </CollapsibleTrigger>
             </SidebarGroupLabel>
