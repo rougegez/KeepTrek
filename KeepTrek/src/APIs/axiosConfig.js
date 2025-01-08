a408c3d8574cb8ccd8515dde68e97f134b962913
rougegez <rougetgc@gmail.com>
2025-01-09 02:48:11 +0800

    user Profile

--- KeepTrek/src/APIs/axiosConfig.js
+++ KeepTrek/src/APIs/axiosConfig.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://127.0.0.1:8000", // Backend URL
+  baseURL: 'http://127.0.0.1:8000',
+  
+  withCredentials: true // Add this if you're using cookies/sessions
 });
 
 // Automatically add Authorization header if token exists
