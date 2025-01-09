3fec96880812e167868242c9fffd4015ea579b19
rougegez <rougetgc@gmail.com>
2025-01-10 03:47:59 +0800

    user edit

--- KeepTrek/src/APIs/users.js
+++ KeepTrek/src/APIs/users.js
@@ -28,34 +28,3 @@ export const getUserProfile = async (userId) => {
     throw error;
   }
 };
-
-export const updateUserProfile = async (username, email) => {
-  console.log('Updating profile with:', { username, email });
-  const token = localStorage.getItem("token"); // Log the payload
-  try {
-    const response = await axios.put(`/users/profile`, null ,{
-      params: {username: username, // Send null if username is not provided
-               email: email },
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      // Send null if email is not provided
-    });
-    console.log('Profile updated:', response.data);
-    return response.data;
-  } catch (error) {
-    // Handle errors and provide feedback similar to your other functions
-    if (error.response) {
-      console.error('Error response:', error.response.data);
-      const { status, data } = error.response;
-      if (status === 404) {
-        throw new Error("User not found");
-      } else if (status === 400) {
-        throw new Error(data.detail || "Invalid user data");
-      } else if (status === 422) {
-        throw new Error(data.detail || "Invalid data format");
-      }
-    }
-    throw new Error("Failed to update user profile");
-  }
-};
