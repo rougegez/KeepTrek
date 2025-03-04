c1fc9c7b46c1e7416d98a0f7c1a0aeaac59f50c4
rougegez <rougetgc@gmail.com>
2025-03-04 21:31:54 +0800

    Update axiosConfig.js

--- KeepTrek/src/APIs/axiosConfig.js
+++ KeepTrek/src/APIs/axiosConfig.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseURL = import.meta.env.VITE_API_URL || 'https://keeptrek-backend.onrender/';
+const baseURL = import.meta.env.VITE_API_URL || 'https://keeptrek-backend.onrender.com/';
 
 const axiosInstance = axios.create({
   baseURL,
