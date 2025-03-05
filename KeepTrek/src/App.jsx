e051cc8d0c83e7f9afd152a19fa36f7ce7cf7bba
rougegez <rougetgc@gmail.com>
2025-03-05 20:09:44 +0800

    --

--- KeepTrek/src/App.jsx
+++ KeepTrek/src/App.jsx
@@ -14,7 +14,7 @@ import PrivateRoute from "./components/PrivateRoute/PrivateRoute.jsx";
 import ItineraryWL from "./components/ItineraryWL/ItineraryWL.jsx";
 import Wishlist from "./components/Wishlist/Wishlist.jsx";
 import { ProfilePage } from "./components/profilePage/profilePage.jsx";
-import { TripDetailsPage } from "./components/TripDetails/TripDetailsPage.jsx";
+// import { TripDetailsPage } from "./components/TripDetails/TripDetailsPage.jsx";
 import { GrpSchedule } from "./components/GrpSchedule/GrpSchedule.jsx";
 import Modal from "./components/Authentication/Modal";
 import LoginForm from "./components/Authentication/login/login-form";
@@ -55,7 +55,7 @@ function App() {
                   >
                     <Route path="/yourTrips" element={<YourTrips />} />
                     <Route path="/expenses/:tripID" element={<MainExpensePage />} />
-                    <Route path="/trip-details" element={<TripDetailsPage />} />
+                    {/* <Route path="/trip-details" element={<TripDetailsPage />} /> */}
                     <Route path="/itineraryWL/:tripID" element={<ItineraryWL />} />
                     <Route path="/schedule/:tripID" element={<GrpSchedule />} />
                     <Route path="/wishlist/:tripID" element={<Wishlist />} />
