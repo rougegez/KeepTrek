799a6d9fe109c7dd6db6ba241d60c947490f6f7a
rougegez <rougetgc@gmail.com>
2025-03-05 19:30:07 +0800

    --

--- KeepTrek/src/components/Expenses/expenseContext.jsx
+++ KeepTrek/src/components/Expenses/expenseContext.jsx
@@ -98,7 +98,7 @@ export function ExpensesProvider ({ children }) {
           totalUser: tripData.user_expense || 0,
           userBalance: tripData.user_balance || 0
         },
-        balances: tripData.balance_map || {},
+        balances: tripData.adjusted_map || {},
         settledDebts: tripData.settled_debts || [],
         budgets: tripData.budgets || []
       };
