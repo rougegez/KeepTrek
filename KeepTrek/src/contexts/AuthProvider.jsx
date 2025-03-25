1303d9fe591861baea9b99301bc2168fe055e047
ProfileBryan <80462985+ProfileBryan@users.noreply.github.com>
2025-03-25 17:18:26 +0800

    added comment

--- KeepTrek/src/contexts/AuthProvider.jsx
+++ KeepTrek/src/contexts/AuthProvider.jsx
@@ -44,7 +44,7 @@ export function AuthProvider({ children }) {
 
   const googleLogin = useCallback(() => {
     // URL of your backend endpoint that starts the Google OAuth flow
-    const googleLoginUrl = "https://keeptrek-backend.onrender.com/auth/google-login";
+    const googleLoginUrl = "https://keeptrek-backend.onrender.com/auth/google-login"; //http://localhost:8000/auth/google-login
     const width = 500;
     const height = 600;
     const left = (window.innerWidth - width) / 2;
